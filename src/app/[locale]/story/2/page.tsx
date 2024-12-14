fc7d635f74011cc712bf3f64f4a5cb6ff4b33914
TK <klarrrr17@gmail.com>
2024-12-14 22:22:58 +0700

    fix: change to use media storage server

--- src/app/[locale]/story/2/page.tsx
+++ src/app/[locale]/story/2/page.tsx
@@ -1,40 +1,40 @@
-"use client"
+"use client";
+
 import NavBarSound from "@/components/SoundNavBar";
 import { useRef, useEffect } from "react";
 import { useRouter } from "@/navigation";
 const Story2 = () => {
-    const router = useRouter();
-    const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-    useEffect(() => {
-        const videoElement = videoRef.current;
-        if (videoElement) {
-            const handleEnded = () => {
-                router.push('/story/3')
-            };
-            videoElement.addEventListener('ended', handleEnded);
-            return () => {
-                videoElement.removeEventListener('ended', handleEnded);
-            };
-        }
-    }, []);
-    return (
-        <div className="w-full min-h-[100dvh] p-10 bg-center bg-no-repeat relative">
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      const handleEnded = () => {
+        router.push("/story/3");
+      };
+      videoElement.addEventListener("ended", handleEnded);
+      return () => {
+        videoElement.removeEventListener("ended", handleEnded);
+      };
+    }
+  }, []);
+  return (
+    <div className="w-full min-h-[100dvh] p-10 bg-center bg-no-repeat relative">
+      <div className="z-10 relative">
+        <NavBarSound></NavBarSound>
+      </div>
 
-            <div className="z-10 relative">
-                <NavBarSound></NavBarSound>
-            </div>
- 
-            <video
-                ref={videoRef}
-                className="z-0 absolute top-0 left-0 w-full h-full object-cover"
-                src="/STORY 2/STORY 2.mp4"
-                autoPlay
-                muted
-                playsInline
-            />
-        </div>
-    )
-}
+      <video
+        ref={videoRef}
+        className="z-0 absolute top-0 left-0 w-full h-full object-cover"
+        src="https://swipe-christmas-2024-asset.vercel.app/STORY%202/STORY%202.mp4"
+        autoPlay
+        muted
+        playsInline
+      />
+    </div>
+  );
+};
 
-export default Story2
+export default Story2;
